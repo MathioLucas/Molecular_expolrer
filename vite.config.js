3650b59280d63789cef2a5cd13d00967e9cef321
Kin Man <silverhairkin@gmail.com>
2025-05-21 02:34:36 +0000

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // or whichever framework you're using
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     allowedHosts: ["lcfg27-4321.csb.app"],
   },
